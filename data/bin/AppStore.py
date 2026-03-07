3ee93d8fc21fc08735b782dc7cd7942d7f89a430
Mit33 <dupart.timothee@notredamesevigne.fr>
2026-03-07 13:45:43 +0100

    Fix indentation in AppStore.py

--- data/bin/AppStore.py
+++ data/bin/AppStore.py
@@ -68,7 +68,7 @@ def update(screen:pygame.Surface,eventGet:list[pygame.event.Event]):
                             with open("data/bin/.apps","a+") as file:
                                 f = file.tell()
                                 file.seek(0)
-								apps = file.read()
+                                apps = file.read()
                                 if "data/bin/"+app+":" not in file.read():
                                     file.seek(f)
                                     file.write(("\n" if apps else "") + "data/bin/"+app+":data/bin/Python.png")
